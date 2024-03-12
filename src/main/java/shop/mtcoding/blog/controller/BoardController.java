f203138b70ec73c9f5f0a044fb35a21e3d6d3fe1
Pakchan <1625787@donga.ac.kr>
2024-03-12 09:49:04 +0900

    초기세팅완료

---
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "save-form";
+    }
+
+    @GetMapping("/board/1")
+    public String detail() {
+        return "board/detail";
+    }
+}
