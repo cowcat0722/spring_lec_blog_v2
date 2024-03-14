4486ab52e34a4ff2b64f1ffacd6ecbac8fa1633a
Pakchan <1625787@donga.ac.kr>
2024-03-14 12:24:21 +0900

    게시글 목록보기

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -4,11 +4,53 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog.user.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    public List<Board> findAllV2() {
+        String q1 = "select b from Board b order by b.id desc";
+        List<Board> boardList1 = em.createQuery(q1, Board.class).getResultList();
+
+        // Board에 글을 쓴 유저의 아이디 배열
+        int[] ids = boardList1.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+
+        // 동적 쿼리 작성
+        String q2 = "select u from User u where u.id in (";
+        for (int i=0; i<ids.length; i++){
+            if(i==ids.length-1){
+                q2 = q2 + ids[i]+")";
+            }else{
+                q2 = q2 + ids[i]+",";
+            }
+        }
+//        System.out.println("q2_test : "+q2);
+        // 글을 작성한 유저의 정보 가져오기
+        List<User> userList = em.createQuery(q2, User.class).getResultList();
+
+        // 스트림 GPT사용
+        List<Board> boardList2 = boardList1.stream()
+                .map(board -> {
+                    board.setUser(userList.stream()
+                            .filter(user -> user.getId() == board.getUser().getId())
+                            .findFirst().orElse(null));
+                    return board;
+                })
+                .collect(Collectors.toList());
+
+        return boardList2;
+    }
+
+    public List<Board> findAll() {
+        Query query = em.createQuery("select b from Board b order by b.id desc", Board.class);
+        return query.getResultList();
+    }
 
     public Board findByIdJoinUser(int id) {
         Query query = em.createQuery("select b from Board b join fetch b.user u where b.id = :id", Board.class);
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -5,12 +5,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(BoardRepository.class)
 @DataJpaTest
 public class BoardRepositoryTest {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Test
+    public void findAllV2_test() {
+
+        List<Board> boardList = boardRepository.findAllV2();
+        System.out.println(boardList);
+
+    }
+
+    @Test
+    public void randomquery_test(){
+        int[] ids = {5,1,2,3,4};
+
+        // select u from User u where u.id in (?,?);
+        String q = "select u from User u where u.id in (";
+        for (int i=0; i<ids.length; i++){
+            if(i==ids.length-1){
+                q = q + ":id"+ ids.length+")";
+            }else{
+                q = q + ":id"+(i+1)+",";
+            }
+        }
+        System.out.println("randomquery_test : "+q);
+    }
+
+    @Test
+    public void findAll_lazyLoading_test() {
+        List<Board> boardList = boardRepository.findAll();
+
+        int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+        for (int i : userIds) {
+            System.out.println(i);
+        }
+        // select * from user_tb where id in (3,2,1,1);
+        // select * from user_tb where id in (3,2,1);
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        boardRepository.findAll();
+        // then
+
+    }
 
     @Test
     public void findById_test() {
