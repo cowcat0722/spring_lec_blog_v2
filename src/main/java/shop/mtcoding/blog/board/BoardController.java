2cbc3138bf4e8b35f4784b8c7c2e1ad7b272133b
Pakchan <1625787@donga.ac.kr>
2024-03-15 09:16:51 +0900

    게시글 작성하기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,12 +1,13 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -14,11 +15,12 @@ import java.util.List;
 @Controller
 public class BoardController {
     private final BoardRepository boardRepository;
+    private final HttpSession session;
 
     @GetMapping("/")
     public String index(HttpServletRequest req) {
         List<Board> boardList = boardRepository.findAllV2();
-        req.setAttribute("boardList",boardList);
+        req.setAttribute("boardList", boardList);
         return "index";
     }
 
@@ -35,13 +37,14 @@ public class BoardController {
     }
 
     @PostMapping("/board/save")
-    public String save() {
-
+    public String save(BoardRequest.SaveDTO reqDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        boardRepository.save(reqDTO.toEntity(sessionUser));
         return "redirect:/";
     }
 
     @PostMapping("/board/{id}/delete")
-    public String save(@PathVariable Integer id) {
+    public String delete(@PathVariable Integer id) {
 
         return "redirect:/";
     }
--- src/main/java/shop/mtcoding/blog/board/BoardRequest.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -1,7 +1,22 @@
 package shop.mtcoding.blog.board;
 
 import lombok.Data;
+import shop.mtcoding.blog.user.User;
 
 public class BoardRequest {
 
+    @Data
+    public static class SaveDTO {
+        private String title;
+        private String content;
+
+        // DTO를 클라이언트로 부터 받아서 PersistContext에 전달하기 위해 사용
+        public Board toEntity(User user) { // user는 세션 넣어주면 됨
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .user(user)
+                    .build();
+        }
+    }
 }
