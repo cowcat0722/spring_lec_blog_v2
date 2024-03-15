aae31df0e7f083e26a860f024e425a3f01da567a
Pakchan <1625787@donga.ac.kr>
2024-03-15 09:23:30 +0900

    게시글 삭제하기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -45,7 +45,7 @@ public class BoardController {
 
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id) {
-
+        boardRepository.deleteById(id);
         return "redirect:/";
     }
 
