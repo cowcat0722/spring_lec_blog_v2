7034a99e9a45adbab4817641edf66c0826b629af
Pakchan <1625787@donga.ac.kr>
2024-03-17 21:20:02 +0900

    게시글 작성하기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,22 +1,25 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
-    private final BoardRepository boardPersistRepository;
+    private final BoardRepository boardRepository;
+    private final HttpSession session;
 
     @GetMapping("/")
     public String index(HttpServletRequest req) {
-        List<Board> boardList = boardPersistRepository.findALL();
+        List<Board> boardList = boardRepository.findALL();
         req.setAttribute("boardList",boardList);
         return "index";
     }
@@ -28,20 +31,21 @@ public class BoardController {
 
     @GetMapping("/board/{id}")
     public String detail(@PathVariable Integer id, HttpServletRequest req) {
-        Board board = boardPersistRepository.findByIdJoinUser(id);
+        Board board = boardRepository.findByIdJoinUser(id);
         req.setAttribute("board",board);
         return "board/detail";
     }
 
     @PostMapping("/board/save")
-    public String save() {
-
+    public String save(BoardRequest.SaveDTO reqDTO) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        boardRepository.save(reqDTO.toEntity(sessionUser));
         return "redirect:/";
     }
 
     @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id) {
-        boardPersistRepository.deleteById(id);
+        boardRepository.deleteById(id);
         return "redirect:/";
     }
 
--- src/main/java/shop/mtcoding/blog/board/BoardRequest.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -1,8 +1,22 @@
 package shop.mtcoding.blog.board;
 
 import lombok.Data;
+import shop.mtcoding.blog.user.User;
 
 public class BoardRequest {
 
+    @Data
+    public static class SaveDTO {
+        private String title;
+        private String content;
 
+        // 빌더 패턴
+        public Board toEntity(User user) {
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .user(user)
+                    .build();
+        }
+    }
 }
