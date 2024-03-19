a462b263719382ec185e525b2d48018f66636a27
Pakchan <1625787@donga.ac.kr>
2024-03-19 16:17:19 +0900

    댓글 삭제

--- src/main/java/shop/mtcoding/blog/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.blog.user.User;
 
@@ -17,5 +18,12 @@ public class ReplyController {
         User sessionUser = (User) session.getAttribute("sessionUser");
         replyService.댓글쓰기(reqDTO, sessionUser);
         return "redirect:/board/" + reqDTO.getBoardId();
+    }
+
+    @PostMapping("/reply/{id}/delete")
+    public String delete(@PathVariable int id) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        int boardId = replyService.댓글삭제(id, sessionUser.getId());
+        return "redirect:/board/"+boardId;
     }
 }
--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
@@ -22,5 +23,19 @@ public class ReplyService {
         Reply reply = reqDTO.toEntity(sessionUser, board);
 
         replyJPARepository.save(reply);
+    }
+
+    public int 댓글삭제(int replyId, int sessionUserId) {
+        Reply reply = replyJPARepository.findById(replyId)
+                .orElseThrow(() -> new Exception404("없는 댓글을 삭제할 수 없어요"));
+
+        int boardId = reply.getBoard().getId();
+
+        if (sessionUserId != reply.getUser().getId()) {
+            throw new Exception403("댓글을 삭제 할 권한이 없습니다.");
+        }
+
+        replyJPARepository.deleteById(replyId);
+        return boardId;
     }
 }
