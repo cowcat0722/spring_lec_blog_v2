bb74ca7b679316c5f2a494da2480ca1259dcfd02
Pakchan <1625787@donga.ac.kr>
2024-03-19 16:38:22 +0900

    게시글 삭제

--- src/main/java/shop/mtcoding/blog/board/BoardService.java
+++ src/main/java/shop/mtcoding/blog/board/BoardService.java
@@ -35,4 +35,15 @@ public class BoardService {
                 .orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다."));
         return board;
     }
+
+    @Transactional
+    public void deleteById(Integer id, int sessionUserId) {
+        Board board = findById(id);
+
+        if (sessionUserId != board.getUser().getId()) {
+            throw new Exception403("게시글을 삭제 할 권한이 없습니다.");
+        }
+
+        boardJPARepository.deleteById(id);
+    }
 }
